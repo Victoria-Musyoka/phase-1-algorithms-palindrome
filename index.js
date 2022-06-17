b1ca8647bdf2af8e6ac996d6f43381da68e225c8
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-17 17:53:56 +0300

    completing assignment

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
+function reversed(Word){
+  const wordArray=word.split("")
+  const reversedWordArray= wordArray.reverse()
+  const reversedWord= reversedWord.join("")
+  return reversedWord;
+}
 function isPalindrome(word) {
   // Write your algorithm here
+
+const reversedWord = reverse(word)
+if (word === reversedWord){
+  return true
+} else {
+  return false
 }
+}
+
 
 /* 
   Add your pseudocode here
 */
+input: "racecar"
+output: true
 
+input: "robot"
+output: false
 /*
   Add written explanation of your solution here
 */
+// if the word matches with its reverse, then it is true. If not, it is false.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
